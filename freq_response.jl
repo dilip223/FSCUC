1bb931d71626e3a67111b248eb650e9b26d3e1a5
dilip223 <pandit.dilip23@gmail.com>
2022-07-18 11:02:11 -0500

    cleanup

--- freq_response.jl
+++ freq_response.jl
@@ -1,4 +1,5 @@
 function freq_initialize(instance::UnitCommitmentInstance)
+
    H_base = 3 .+ 6 .* rand(Float64,(length(instance.units)))
    R_base = 0.03 .+ 0.08 .* rand(Float64,(length(instance.units)))
    K_base =  0.8 .+ 0.4 .* rand(Float64,(length(instance.units)))
@@ -9,6 +10,7 @@ function freq_initialize(instance::UnitCommitmentInstance)
             for g in 1:length(instance.units)]
    DData = DynamicsData(R_base,K_base,F_base,D_base,H_base,pu,T_r)
    return DData
+
 end
 
 function freq_test(instance::UnitCommitmentInstance,
@@ -17,6 +19,7 @@ function freq_test(instance::UnitCommitmentInstance,
                   rocof::Vector{Float64},
                   DData::DynamicsData,
                   n_cont::Int)
+
       T = instance.time
       for t in 1:T
          R_T=0
@@ -32,14 +35,10 @@ function freq_test(instance::UnitCommitmentInstance,
                                   for g in 1:length(instance.units))
          temp = [value(model[:is_on][instance.units[g].name,t])
                               for g in 1:length(instance.units)]
-         # print(temp)
-         #temp = ones((length(instance.units),1))
-         temp[ind_max].=0
+         temp[ind_max].= 0
             for g in 1:length(instance.units)
-                 #on_status = value(model[:is_on][instance.units[g].name,t])
                  on_status = temp[g]
                  const_par = instance.units[g].max_power[T]/sum(instance.units[g].max_power[1] for g in 1:length(instance.units))
-                 #tot_pow[t] = instance.units[g].max_power[T].*value(model[:is_on][instance.units[g].name,t])
                  R_T = R_T+(DData.K[g]/DData.R[g])*on_status*const_par
                  F_T = F_T+(DData.F[g]*DData.K[g]/DData.R[g])*on_status*const_par
                  H_T = H_T+ DData.H[g]*on_status*const_par
@@ -52,5 +51,6 @@ function freq_test(instance::UnitCommitmentInstance,
          f_max[t] = pow_max/(D_T+R_T)*(1+sqrt(DData.T_r*(R_T-F_T)/(2*H_T))*
                      exp(-zeta*omega_n*t_max))
          rocof[t] = pow_max/(2*H_T)
+
        end
 end
