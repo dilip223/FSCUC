4dc21f6907421087ee707e27d71b3455d4006d03
dilip223 <pandit.dilip23@gmail.com>
2022-07-22 13:08:00 -0500

    minor changes

--- freq_response.jl
+++ freq_response.jl
@@ -32,7 +32,7 @@ function freq_test(instance::UnitCommitmentInstance,
 
          pow_mat = [(instance.units[g].min_power[t]+value(model[:prod_above][instance.units[g].name,t])).*value(model[:is_on][instance.units[g].name,t]) for g in 1:length(instance.units)]
          pow_max = sort(pow_mat,rev=true)[1:n_cont]
-         
+
          ind_max = sortperm(pow_mat,rev=true)[1:n_cont]
 
          pow_max = sum(pow_max)./sum(instance.units[g].max_power[t]
@@ -57,20 +57,4 @@ function freq_test(instance::UnitCommitmentInstance,
          rocof[t] = pow_max/(2*H_T)
 
        end
-end
-
-function plot_figures(instance::UnitCommitmentInstance,
-                      model:: JuMP.Model,
-                      f_max:: Vector{Float64},
-                      rocof:: Vector{Float64})
-
-   #plot(rocof.*60, legend=false, xlabel= "Time (hours)", ylabel= "RoCoF (Hz/s)")
-   savefig("rocof_plot.png")
-   tot_units =zeros((instance.time,1))
-   for i in 1:instance.time
-          tot_units[i] = sum(value(model[:is_on][instance.units[g].name,i]) for g in 1:length(instance.units))
-   end
-   #plot(tot_units,legend=false, xlabel= "Time (hours)", ylabel= "No. of units online")
-   savefig("tot_units_commit.png")
-
 end
